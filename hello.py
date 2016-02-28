6248113fa1a78b2f8cf15dea1c007a808a769e47
victoryagudaev@gmail.com <victoryagudaev@gmail.com>
2016-02-28 19:36:57 +0400

    8

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ def wsgi_application(environ, start_response):
 	param = urlparse.parse_qs(environ['QUERY_STRING'],1)	
 
 	for i in range(len(param)):
-		color[i] += '\r\n'
+		param[i] += '\r\n'
 	
 	
 	headers = [
