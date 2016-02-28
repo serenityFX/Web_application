f5d80e258bd93a7bd11b4520039dd3b4a56a137e
serenityFX <victoryagudaev@gmail.com>
2016-02-28 15:50:27 +0000

    3

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-#sudo gunicorn -b 0.0.0.0:8080 /home/box/web/hello:application
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application
