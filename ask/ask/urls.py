d88e50b9b680d67f77012c447ab4b498f5504aad
serenityFX <victoryagudaev@gmail.com>
2016-03-05 19:05:47 +0000

    3

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
+     url(r'^$', include("qa.urls")),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
